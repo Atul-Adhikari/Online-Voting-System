fa828600ce88ea53b3d7403908b2914a2e819952
Ananda Dhakal <anandadhakal13@gmail.com>
2025-04-02 08:47:58 +0545

    add additional user fields

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,27 @@ const UserSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    address: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
     email: {
       type: String,
       required: true,
