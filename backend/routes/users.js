0552075b0ccfc07f4e4c747ab5c937e919dab3e3
Ananda Dhakal <anandadhakal13@gmail.com>
2025-04-23 11:29:31 +0545

    additional improvements

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,15 +13,8 @@ const {
   getAllUsers
 } = require("../controllers/users");
 
-const emailRegex =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/;
-const dateOfBirthRegex = /^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-
-//Register user
 router.post("/register", registerUser);
 
-//Login user
 router.post("/login", loginUser);
 
 router.get("/me", authMiddleware, getUser);
